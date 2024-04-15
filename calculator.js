7e6a3420ba98129f47729dd5ed0650c86429bf8c
SbongileM <Sbongilep4@gmail.com>
2024-04-15 20:42:56 +0200

    Add Javascript file

---
+++ calculator.js
@@ -0,0 +1,24 @@
+function pressed(button) {
+
+  }
+
+  function root() {
+
+  }
+
+  function percent() {
+
+  }
+
+  function square() {
+
+
+  }
+
+  function calculate() {
+
+  }
+
+  function dec_point() {
+
+  }
